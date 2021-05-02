1aea48f0b5f87fffdadd883cb11bba17504f4820
wcc723 <agito723@gmail.com>
2021-05-02 13:54:42 +0800

    上傳圖片及多圖範例

--- src/components/ProductModal.vue
+++ src/components/ProductModal.vue
@@ -206,6 +206,10 @@ export default {
   watch: {
     product() {
       this.tempProduct = this.product;
+      // 多圖範例
+      if (!this.tempProduct.images) {
+        this.tempProduct.images = [];
+      }
     },
   },
   data() {
@@ -220,6 +224,18 @@ export default {
     },
     hideModal() {
       this.modal.hide();
+    },
+    uploadFile() {
+      const uploadedFile = this.$refs.fileInput.files[0];
+      const formData = new FormData();
+      formData.append('file-to-upload', uploadedFile);
+      const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/upload`;
+      this.$http.post(url, formData).then((response) => {
+        console.log(response.data);
+        if (response.data.success) {
+          this.tempProduct.imageUrl = response.data.imageUrl;
+        }
+      });
     },
   },
   mounted() {
