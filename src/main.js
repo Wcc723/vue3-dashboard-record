450aa7c0dd7e750ac58a938746d3776f9b0ade91
wcc723 <agito723@gmail.com>
2021-05-16 13:09:47 +0800

    購物車列表及 Bootstrap Icons

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import VueAxios from 'vue-axios';
 import Loading from 'vue3-loading-overlay';
 import 'vue3-loading-overlay/dist/vue3-loading-overlay.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import App from './App.vue';
 import router from './router';
--- src/views/UserCart.vue
+++ src/views/UserCart.vue
@@ -46,6 +46,68 @@
         </table>
       </div>
       <!-- 購物車列表 -->
+      <div class="col-md-5">
+        <div class="sticky-top">
+          <table class="table align-middle">
+            <thead>
+              <tr>
+                <th></th>
+                <th>品名</th>
+                <th style="width: 110px">數量</th>
+                <th>單價</th>
+              </tr>
+            </thead>
+            <tbody>
+            <template v-if="cart.carts">
+              <tr v-for="item in cart.carts" :key="item.id">
+                <td>
+                  <button type="button" class="btn btn-outline-danger btn-sm"
+                          :disabled="status.loadingItem === item.id"
+                          @click="removeCartItem(item.id)">
+                    <i class="bi bi-x"></i>
+                  </button>
+                </td>
+                <td>
+                  {{ item.product.title }}
+                  <div class="text-success" v-if="item.coupon">
+                    已套用優惠券
+                  </div>
+                </td>
+                <td>
+                  <div class="input-group input-group-sm">
+                    <input type="number" class="form-control"
+                          v-model.number="item.qty">
+                    <div class="input-group-text">/ {{ item.product.unit }}</div>
+                  </div>
+                </td>
+                <td class="text-end">
+                  <small v-if="cart.final_total !== cart.total" class="text-success">折扣價：</small>
+                  {{ $filters.currency(item.final_total) }}
+                </td>
+              </tr>
+            </template>
+            </tbody>
+            <tfoot>
+            <tr>
+              <td colspan="3" class="text-end">總計</td>
+              <td class="text-end">{{ $filters.currency(cart.total) }}</td>
+            </tr>
+            <tr v-if="cart.final_total !== cart.total">
+              <td colspan="3" class="text-end text-success">折扣價</td>
+              <td class="text-end text-success">{{ $filters.currency(cart.final_total) }}</td>
+            </tr>
+            </tfoot>
+          </table>
+          <div class="input-group mb-3 input-group-sm">
+            <input type="text" class="form-control" v-model="coupon_code" placeholder="請輸入優惠碼">
+            <div class="input-group-append">
+              <button class="btn btn-outline-secondary" type="button" @click="addCouponCode">
+                套用優惠碼
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -59,6 +121,8 @@ export default {
       status: {
         loadingItem: '', // 對應品項 id
       },
+      cart: {},
+      coupon_code: '',
     };
   },
   methods: {
@@ -87,9 +151,19 @@ export default {
           console.log(res);
         });
     },
+    getCart() {
+      const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/cart`;
+      this.isLoading = true;
+      this.$http.get(url).then((response) => {
+        console.log(response);
+        this.cart = response.data.data;
+        this.isLoading = false;
+      });
+    },
   },
   created() {
     this.getProducts();
+    this.getCart();
   },
 };
 </script>
